24e86146052f77711433240c7cffbd2a28f5e7fa
Omar Mohamed <119631276+omarmohamedreda@users.noreply.github.com>
2024-01-21 03:05:10 +0200

    Update 4-rectangle.js

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -1,28 +1,25 @@
-if (w && h > 0)
-{
-    this.width = w
-    this.height = h   
-    return this.width, this.width
-}
-
-print()
-{
-    for (let i=0; i<this.height; i++)
-    {
-        console.log('X'.repeat(this.width))
+#!/usr/bin/node
+class Rectangle {
+    constructor(w, h) {
+        if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+            // Create an empty object if width or height is invalid
+            return "Rectangle {}";
+        }
+        this.width = w;
+        this.height = h;
     }
+    print() {
+        for (let i = 0; i < this.height; i++) {
+          console.log('X'.repeat(this.width));
+        }
+      }
+      rotate() {
+        [this.width, this.height] = [this.height, this.width];
+      }
+    
+      double() {
+        this.width *= 2;
+        this.height *= 2;
+      }
 }
-rotate()
-{
-const rot_h = this.width
-this.width = this.height
-this.height = rot_h
-}
-double()
-{
-this.height = this.height * 2
-this.width = this.width * 2
-}
-;
-
 module.exports = Rectangle;
