b437a88988cf3f0a8e5c2b973bbcac051f2be36f
Omar Mohamed <119631276+omarmohamedreda@users.noreply.github.com>
2024-01-21 03:08:10 +0200

    Create 5-main.js

---
+++ javascript_objects_scopes_closures/5-main.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+class Square extends Rectangle {
+    constructor(side) {
+        super(side, side);
+    }
+}
+module.exports = Square;
