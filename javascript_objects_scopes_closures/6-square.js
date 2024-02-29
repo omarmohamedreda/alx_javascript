7bf0d6ca03fc390f91e337d1a0e75e0175475863
Omar Mohamed <119631276+omarmohamedreda@users.noreply.github.com>
2024-02-29 08:22:35 +0200

    Update 6-square.js

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,19 +1,31 @@
-#!/usr/bin/node
-const Rectangle = require('./5-square'); // Importing Rectangle, but not using it in the code
+const Rectangle = require('./4-rectangle.js');
+//5-square
 
-class Square extends Rectangle { // Extending Rectangle instead of Square
-  constructor(size) { // Using constructor instead of super for initializing properties
-    super(size, size); // Calling super() with the appropriate arguments
-  }
+class Square extends Rectangle {
+    constructor(size){
+        super(size, size);
+    }
+    
+    charPrint(c) {
+        if (typeof c === 'undefined') {
+            for (let i=0; i <this.height; i++) {
+                for (let j=0; j <this.width; j++) {
+                    process.stdout.write('X');
+                }            
+                process.stdout.write('\n');
+            }
+        }
+        
+        else {
+            for (let i=0; i <this.height; i++) {
+                for (let j=0; j <this.width; j++) {
+                    process.stdout.write(c);
+                }            
+                process.stdout.write('\n');
+            }
+        }
+    }
 
-  charPrint(c) {
-    if (c === undefined) {
-      c = 'X';
-    }
-    for (let i = 0; i < this.height; i++) {
-      console.log(c.repeat(this.width));
-    }
-  }
 }
 
 module.exports = Square;
